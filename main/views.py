163cba621340d84500f060302b8a7dbf5f0acf69
Luknam <rtreetanakorn@gmail.com>
2020-05-03 15:02:55 +0700

    unboundlocalerror

--- main/views.py
+++ main/views.py
@@ -1,28 +1,83 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Tutorial
-from django.contrib.auth.forms import UserCreationForm
+from .models import Tutorial, TutorialCategory, TutorialSeries
+from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login,logout, authenticate
+from django.contrib import messages 
+from .forms import NewUserForm
+
+def single_slug(request, single_slug):
+	categories=[c.category_slug for c in TutorialCategory.objects.all()]
+	if single_slug in categories:
+		matching_series=TutorialSeries.objects.filter(tutorial_category__category_slug=single_slug)
+			
+		series_urls={}
+		for m in matching_series.all():
+			part_one=Tutorial.objects.filter(tutorial_series__tutorial_series=m.tutorial_series).earliest("tutorial_published")
+			series_urls[m]=part_one.tutorial_slug
+
+
+		return render(request,
+				      "main/category.html",
+				      {"part_ones":series_urls})
+
+		tutorials=[t.tutorial_slug for t in Tutorial.objects.all()]
+	if single_slug in tutorials:
+		return HttpResponse(f"{single_slug} is a tutorial!!!")
+	return HttpResponse(f"{single_slug} does not correspond to anything.")
+
 
 # Create your views here.
 def homepage(request):
 	return render(request=request,
-				  template_name="main/home.html",
-				  context={"tutorials": Tutorial.objects.all})
+				  template_name="main/categories.html",
+				  context={"categories": TutorialCategory.objects.all})
 	#HttpResponse("Wow this is an <strong>awesome</strong> tutorial") 
 
 def register(request):
 	if request.method=="POST":
-		form=UserCreationForm(request.POST)
+		form=NewUserForm(request.POST)
 		if form.is_valid():
 			user=form.save()
+			username=form.cleaned_data.get('username')
+			messages.success(request, f"New Accout Created: {username}")
 			login(request, user)
+			messages.info(request, f"You are now logged in as {username}")
 			return redirect("main:homepage")
 		else:
 			for msg in form.error_messages:
-				print(form.error_messages[msg])
+				messages.error(request,f"{msg}: {form.error_messages[msg]}")
 
-	form= UserCreationForm
+	form= NewUserForm
 	return render(request,
 				  "main/register.html",
 				  context={"form": form})
+
+def logout_request(request):
+	logout(request)
+	messages.info(request,"logged out successfully!")
+	return redirect("main:homepage")
+
+def login_request(request):
+	if request.method=="POST":
+		form=AuthenticationForm(request, data=request.POST)
+		if form.is_valid():
+			user=form.save()
+			username=form.cleaned_data.get('username')
+			password=form.cleaned_data.get('password')
+			user=authenticate(username=username, password=password)
+			if user is not None:
+				login(request, user)
+				messages.info(request, f"You are now logged in as {username}")
+				return redirect("main:homepage")
+			else:
+				messages.error(request,"Invalid username or password")
+	else:
+		messages.error(request,"Invalid username or password")
+
+
+	form=AuthenticationForm()
+	return render(request,
+				  "main/login.html",
+				  {"form":form})
+
